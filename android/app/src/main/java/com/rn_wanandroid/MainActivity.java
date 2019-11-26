b27ef552a6793f84a3dd3e744ae8a9a689832986
WuMeng <541345568@qq.com>
2019-11-26 15:00:02 +0800

    添加Android端的启动页

--- android/app/src/main/java/com/rn_wanandroid/MainActivity.java
+++ android/app/src/main/java/com/rn_wanandroid/MainActivity.java
@@ -1,8 +1,16 @@
 package com.rn_wanandroid;
 
 import com.facebook.react.ReactActivity;
+import android.os.Bundle;  //add
+import org.devio.rn.splashscreen.SplashScreen; //add
 
 public class MainActivity extends ReactActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen.show(this, true);
+        super.onCreate(savedInstanceState);
+    }
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
